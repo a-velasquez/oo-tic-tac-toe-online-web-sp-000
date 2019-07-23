28b578180756e9875b48e6f73a8b4a49c4b11aca
Alex Velasquez <jtinnesco@gmail.com>
2019-07-23 04:38:32 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -36,6 +36,13 @@ class TicTacToe
   end
   
   
-  
+  def valid_move?(board, index)
+    if index.between?(0, 8) && !(position_taken?(board, index))
+    TRUE
+    else
+    FALSE
+    end
+  end
+    
   
 end
