9f0094f5ab2e1a1b82a1b76d620c27f2345cb5ba
Alex Velasquez <jtinnesco@gmail.com>
2019-07-23 04:43:57 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -40,12 +40,14 @@ class TicTacToe
     index.between?(0,8) && !position_taken?(index)
   end
   
-  def turn_count
-    number_of_turns = 0
-    @board.each do |space|
+  def turn_count(board)
+    counter = 0
+    board.each do |space|
     if space == "X" || space == "O"
-        number_of_turns += 1
+    counter += 1
     end
+  end
+    counter
   end
     
   def turn
